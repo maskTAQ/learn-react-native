c3ef6b8caf2f4d4c5c870974986f517c855e90b9
taiaq <50889574@qq.com>
2017-04-24 21:52:44 +0800

    注释了ripple组件的实现方式。

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 ## Ripple
 虽然后来的版本官方实现了这个特效组件 `TouchableNativeFeedback `,但是学会了思路,后期自己也可以实现比较酷的特效,想想就很酷。
 通过[手势](https://github.com/jabez128/jabez128.github.io/issues/1)来触发来调用组件内部的animate的周期函数实现ripple特效。
+  1. 手势操作的api跟源码的版本做了更改。
+
+
+    知识点
+
+  1. [手势操作对应的生命周期](http://reactnative.cn/docs/0.43/panresponder.html#%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)。
+  2. Animated其实就是值驱动试图的刷新,常用的方法就是动画的执行方式,[队列、并行](http://reactnative.cn/docs/0.43/animated.html#%E6%96%B9%E6%B3%95)等等。
+  3. measure方法。
+  ```javascript
+  this.refs.container.measure((x, y, width, height, pageX, pageY)=> {
+      this.setState({
+          size: 2 * (width > height ? width : height),
+          location: {pageX, pageY}
+      })
+  });
+  ```
--- ripple/index.js
+++ ripple/index.js
@@ -1,10 +1,15 @@
+/*
+结构 View.container>View.background+Animated.View
+痛过手势触发 改变View.background 的背景颜色和透明度 + 获取手势触发的位置定位Animated.View的位置 然后动态改变缩放比和加radius属性来实现ripple特效
+*/
 import React, {
     Component,
     StyleSheet,
     PropTypes,
     View,
     Animated,
-    Text
+    Text,
+    PanResponder
 } from 'react-native';
 
 export default class Ripple extends Component {
@@ -21,19 +26,139 @@ export default class Ripple extends Component {
         backgroundOpacity: PropTypes.number
     };
 
-    state = {
-        rippling: false
-    };
-
     constructor(props) {
         super(props);
 
         this.state = {
+            rippling: false,
             scaleValue: new Animated.Value(0.001)
         };
 
-        this._responder = {
-            onStartShouldSetResponder: (e) => true
-        }
+        this._responder = PanResponder.create({
+            //启用手势响应
+            onStartShouldSetPanResponder: (e) => true,
+            //开始手势操作
+            onPanResponderGrant: this._highlight,
+            //结束手势操作
+            onPanResponderRelease: this._handleResponderEnd,
+            // 另一个组件已经成为了新的响应者，所以当前手势将被取消
+            onPanResponderTerminate: this._unHightlight
+        });
+    }
+    render() {
+        const {
+            color,
+            background,
+            rippleOpacity,
+            backgroundOpacity,
+            children,
+            style,
+            ...other
+        } = this.props;
+        const {
+            rippling,
+            size,
+            pageX,
+            pageY,
+            scaleValue,
+            location
+        } = this.state;
+
+        const assignStyle = Object.assign({}, styles.container, style);
+
+        return (
+            <View ref="container" style={assignStyle} {...this._responder} {...other}>
+                <View style={[
+                    styles.background, backgroundColor : rippling
+                        ? background
+                        : 'transparent',
+                    opacity : backgroundOpacity
+                ]}/>
+                <Animated.View style={[
+                  //这里判断了一下location是否存在???
+                    styles.Ripple, location && {
+                        backgroundColor: color,
+                        width: size,
+                        height: size,
+                        top: pageY - location.pageY - size / 2,
+                        left: pageX - location.pageX - size / 2,
+                        borderRadius: size / 2,
+                        opacity: rippleOpacity
+                    }, {
+                        transform: [
+                            {
+                                scale: scaleValue
+                            }
+                        ]
+                    }
+                ]}/>
+                 {children}
+            </View>
+        )
+    }
+    _highlight = (e) => {
+        const {
+            pageX,
+            pageY
+            } = e.nativeEvent;
+
+        this.setState({
+            rippling: true,
+            pageX,
+            pageY
+        });
+
+        !!this.state.size || this._getContainerDimensions();
+
+        Animated.timing(
+            this.state.scaleValue,
+            {
+                toValue: 1,
+                duration: 500
+            }
+        ).start();
+    };
+
+    _unHighlight = () => {
+        this.setState({
+            rippling: false
+        });
+
+        Animated.timing(
+            this.state.scaleValue,
+            {
+                toValue: 0.001,
+                duration: 0
+            }
+        ).start();
+    };
+    _handleResponderEnd = (e) => {
+        var onPress = this.props.onPress;
+        onPress && setTimeout(onPress.bind(this), 0)
+        this._unHighlight();
+    };
+    _getContainerDimensions = () => {
+        this.refs.container.measure((x, y, width, height, pageX, pageY)=> {
+            this.setState({
+                size: 2 * (width > height ? width : height),
+                location: {pageX, pageY}
+            })
+        })
     }
 }
+const styles = {
+    container: {
+        backgroundColor: 'rgba(0,0,0,0)',
+        overflow: 'hidden'
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+    },
+    ripple: {
+        position: 'absolute',
+    }
+};
